d3ab2fb10b0ee27bef501497430513ceade19bd0
Dmitry Yakushev <d.yakushev@gastrofix.com>
2019-02-19 16:18:14 +0300

    Functional test: check that user can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -15,18 +17,35 @@ class NewVisitorTest(unittest.TestCase):
 
 		# User see that page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+
+		# User is presented an input text-box, inviting to enter a to-do item
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+			)
+
+		# User types "Take car from repair"
+		inputbox.send_keys('Take car from repair')
+
+		# When user clicks enter, page updates, and now the page lists
+		# "1: Take car from repear" as an item in to-do list
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Take car from repear' for row in rows)
+			)
+
+		
+		# There is still a text box, inviting user to write another item in to-do list
+
+		# User enters "Write some report tests"
 		self.fail('Finish the test!')
-
-# User is presented a text-box, inviting to enter a to-do item
-
-# User types "Take car from repair"
-
-# When user clicks enter, page updates, and now the page lists
-# "1: Take car from repear" as an item in to-do list
-
-# There is still a text box, inviting user to write another item in to-do list
-
-# User enters "Write some report tests"
 
 # The page updates again and now there are 2 items in to-do list
 
