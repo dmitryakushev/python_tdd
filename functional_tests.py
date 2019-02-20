c05298224891dc4f8f44e06b0883ee15cbc278c3
Dmitry Yakushev <d.yakushev@gastrofix.com>
2019-02-20 17:34:45 +0300

    Refactor FT to use helper method when verify if text is present in a table

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,12 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# User opens homapage of the inline to-do app
 		self.browser.get('http://localhost:8000')
@@ -49,10 +55,8 @@ class NewVisitorTest(unittest.TestCase):
 		time.sleep(1)
 
 		# The page updates again and now there are 2 items in to-do list
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Take car from repair', [row.text for row in rows])
-		self.assertIn('2: Write some report tests', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Take car from repair')
+		self.check_for_row_in_list_table('2: Write some report tests')
 
 		# Site generates a URL to store the to-do list
 		# Info message is showing to user informing about storing to-do list at the URL
