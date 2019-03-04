830afcf3a84ab93f07f93a0f6e217041447fa1ad
Dmitry Yakushev <d.yakushev@gastrofix.com>
2019-03-04 17:04:24 +0300

    Replace sleep wait with a retry loop wait

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,10 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -12,10 +15,18 @@ class NewVisitorTest(LiveServerTestCase):
 		self.browser.quit()
 
 
-	def check_for_row_in_list_table(self, row_text):
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(row_text, [row.text for row in rows])
+	def wait_for_row_in_list_table(self, row_text):
+		start_time = time.time()
+		while True:
+			try:
+				table = self.browser.find_element_by_id('id_list_table')
+				rows = table.find_elements_by_tag_name('tr')
+				self.assertIn(row_text, [row.text for row in rows])
+				return
+			except(AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# User opens homapage of the inline to-do app
@@ -39,8 +50,7 @@ class NewVisitorTest(LiveServerTestCase):
 		# When user clicks enter, page updates, and now the page lists
 		# "1: Take car from repair" as an item in to-do list
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
-		self.check_for_row_in_list_table('1: Take car from repair')
+		self.wait_for_row_in_list_table('1: Take car from repair')
 
 		
 		# There is still a text box, inviting user to write another item in to-do list
@@ -52,8 +62,8 @@ class NewVisitorTest(LiveServerTestCase):
 		time.sleep(1)
 
 		# The page updates again and now there are 2 items in to-do list
-		self.check_for_row_in_list_table('1: Take car from repair')
-		self.check_for_row_in_list_table('2: Write some report tests')
+		self.wait_for_row_in_list_table('1: Take car from repair')
+		self.wait_for_row_in_list_table('2: Write some report tests')
 
 		# Site generates a URL to store the to-do list
 		# Info message is showing to user informing about storing to-do list at the URL
